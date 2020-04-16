66e5583d5acf6e11d3f50ab0ab07435ce2d275c3
Shanti Stewart <stewars2@oregonstate.edu>
2020-04-15 22:19:27 -0700

    Finished average_PSD() -- not tested.

--- feature_calculation/average_PSD.py
+++ feature_calculation/average_PSD.py
@@ -27,41 +27,17 @@ def average_PSD(PSD, bins, sample_freq):
     max_freq = .5 * sample_freq
 
     # calculate average PSD values in frequency bins:
-    PSD_bins = np.zeros(num_bins)
-
-
-
-# Function description: calculate average PSD values in specified frequency bins.
-# Inputs:
-#   PSD_mag_pos = 1D array of magnitude response of PSD
-#       size: [pos_num_samples]
-#   bins = frequency bins of interest
-#       size: [num_bins, 2]
-#   bin_labels = frequency bin labels
-#       size: [num_bins]
-#   sample_freq = sampling frequency of original signal
-#   disp = bool to select whether to display bar graph
-# Outputs:
-#   PSD_bins = 1D array of average PSD values in frequency bins
-#       size: [num_bins]
-def average_bin(PSD_mag_pos, bins, bin_labels, sample_freq, disp):
-    # number of PSD samples:
-    num_samples = len(PSD_mag_pos)
-    # max sampling frequency (Nyquist rate):
-    max_freq = .5 * sample_freq
-    # number of frequency bins:
-    num_bins = len(bins)
-
-    # calculate average power spectral density values in frequency bins:
-    PSD_bins = np.zeros(num_bins)
+    PSD_bins = np.zeros((num_examples, num_channels, num_bins))
     for i in range(num_bins):
-        # left index:
-        left = int(np.floor(num_samples * (bins[i, 0] / max_freq)))
-        # right index:
-        right = int(np.floor(num_samples * (bins[i, 1] / max_freq)))
-        # number of samples in bin:
-        bin_width = right - left
-        PSD_bins[i] = np.sum(PSD_mag_pos[left:right]) / bin_width
+        # left sample index (round down):
+        left = int(np.floor((bins[i, 0] / max_freq) * num_samples))
+        # right sample index (round up):
+        right = int(np.ceil((bins[i, 1] / max_freq) * num_samples))
 
-    # return average power spectral density values in frequency bins:
+        # check that number of samples in bin is non-negative:
+        if right-left < 0:
+            print("Invalid frequency bin.\n")
+        # calculate average:
+        PSD_bins[:, :, i] = np.mean(PSD[:, :, left:right], axis=2)
+
     return PSD_bins
