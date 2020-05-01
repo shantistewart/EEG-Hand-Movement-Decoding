1fc05f259105acb18ca080e3932bda56178137ec
Shanti Stewart <stewars2@oregonstate.edu>
2020-04-30 17:51:44 -0700

    Started unit test for log_normalize().

--- unit_tests/PCA_on_PSD_test.py
+++ unit_tests/PCA_on_PSD_test.py
@@ -8,3 +8,18 @@ from feature_calculation import PCA_on_PSD
 
 # --------------------TESTING log_normalize() FUNCTION--------------------
 print("\n----------TESTING log_normalize() FUNCTION----------\n")
+
+# dimensions of test array:
+num_examples = 3
+num_channels = 2
+num_samples = 4
+# test input array:
+PSD = np.zeros((num_examples, num_channels, num_samples))
+for i in range(num_examples):
+    for j in range(num_channels):
+        for k in range(num_samples):
+            PSD[i, j, k] = (i+1) * (j*num_samples + k + 1)
+print("Test input array:\nSize: ", end="")
+print(PSD.shape)
+print(PSD)
+print("")
