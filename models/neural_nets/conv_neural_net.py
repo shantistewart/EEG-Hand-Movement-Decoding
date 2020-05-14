c9782d576fbfa033d7ca2d189f65d90d3123d45c
Shanti Stewart <stewars2@oregonstate.edu>
2020-05-13 22:20:17 -0700

    Initial creation of build_model().

--- models/neural_nets/conv_neural_net.py
+++ models/neural_nets/conv_neural_net.py
@@ -4,15 +4,20 @@
 import numpy as np
 import matplotlib.pyplot as plotter
 import tensorflow as tf
-from tensorflow import keras
+from tensorflow.keras import layers, models
 
 
-# Hyperparameters:
-# network architecture:
-num_conv_layers = 1
-num_pool_layers = 1
-num_connect_layers = 1
-num_hidden_nodes = 50
-# training:
-num_epochs = 15
-batch_size = 128
+def build_model(input_shape, num_outputs=1, num_conv_layers=1, num_dense_layers=1, num_filters=2, kernel_size=3,
+                pool_size=2, num_hidden_nodes=50):
+    model = models.Sequential()
+
+    # convolutional and max pooling layers:
+    model.add(layers.Conv2D(num_filters, kernel_size, padding='valid', activation='relu', input_shape=input_shape))
+    model.add(layers.MaxPool2D(pool_size, padding='valid'))
+
+    # fully connected layers:
+    model.add(layers.Flatten())
+    model.add(layers.Dense(num_hidden_nodes, activation='relu'))
+    model.add(layers.Dense(num_outputs))
+
+    return model
