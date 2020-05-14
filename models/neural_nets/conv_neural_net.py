7e3bcd5e6d4f3b4703dc365a3d1ab78504b6500f
Shanti Stewart <stewars2@oregonstate.edu>
2020-05-13 22:47:08 -0700

    Added conv_neural_net_test.py

--- models/neural_nets/conv_neural_net.py
+++ models/neural_nets/conv_neural_net.py
@@ -7,11 +7,13 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 
 
-def build_model(input_shape, num_outputs=1, num_conv_layers=1, num_dense_layers=1, num_filters=2, kernel_size=3,
+def build_model(input_shape, num_outputs, num_conv_layers=2, num_dense_layers=1, num_filters=2, kernel_size=3,
                 pool_size=2, num_hidden_nodes=50):
     model = models.Sequential()
 
     # convolutional and max pooling layers:
+    model.add(layers.Conv2D(num_filters, kernel_size, padding='valid', activation='relu', input_shape=input_shape))
+    model.add(layers.MaxPool2D(pool_size, padding='valid'))
     model.add(layers.Conv2D(num_filters, kernel_size, padding='valid', activation='relu', input_shape=input_shape))
     model.add(layers.MaxPool2D(pool_size, padding='valid'))
 
