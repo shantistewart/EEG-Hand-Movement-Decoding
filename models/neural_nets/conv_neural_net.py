6ca4f2ddcdb630421770886b7d9914f0811ea2b1
Shanti Stewart <stewars2@oregonstate.edu>
2020-05-13 21:32:08 -0700

    Started conv_neural_net.py

--- models/neural_nets/conv_neural_net.py
+++ models/neural_nets/conv_neural_net.py
@@ -2,4 +2,17 @@
 
 
 import numpy as np
+import matplotlib.pyplot as plotter
 import tensorflow as tf
+from tensorflow import keras
+
+
+# Hyperparameters:
+# network architecture:
+num_conv_layers = 1
+num_pool_layers = 1
+num_connect_layers = 1
+num_hidden_nodes = 50
+# training:
+num_epochs = 15
+batch_size = 128
