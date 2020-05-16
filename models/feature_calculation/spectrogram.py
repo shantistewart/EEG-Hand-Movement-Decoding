1a4c935a87d43f538149170ed98412a08560143a
Shanti Stewart <stewars2@oregonstate.edu>
2020-05-16 13:00:45 -0700

    Created function outlines.

--- models/feature_calculation/spectrogram.py
+++ models/feature_calculation/spectrogram.py
@@ -2,3 +2,19 @@
 
 
 import numpy as np
+from models.feature_calculation import feature_algorithms
+
+
+def segment_data(X, num_segments, window_size):
+
+    return X
+
+
+def window_data(X, window_size, stride_size):
+
+    return X
+
+
+def create_spectrogram(X, num_windows, sample_freq, max_freq, num_bins):
+
+    return X
