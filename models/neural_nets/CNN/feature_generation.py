e1ea59679d9daa7822010756d0b5b673432cb374
Shanti Stewart <stewars2@oregonstate.edu>
2020-05-18 23:34:29 -0700

    Almost done with generate_examples().

--- models/neural_nets/CNN/feature_generation.py
+++ models/neural_nets/CNN/feature_generation.py
@@ -6,8 +6,12 @@ from models.data_gathering import data4_reader
 from models.feature_calculation import spectrogram
 from models.feature_calculation import feature_algorithms as feature
 
+# class labels (do not modify!):
+LEFT_HAND_LABEL = 0
+RIGHT_HAND_LABEL = 1
 
-# Function description: performs a sliding-window segmentation on raw signal values.
+
+# Function description: generates examples (raw data + class labels).
 # Inputs:
 #   subject_num = number of human subject (1-9)
 #   path_to_file = path to data file
@@ -17,4 +21,22 @@ from models.feature_calculation import feature_algorithms as feature
 # Outputs:
 def generate_examples(subject_num, path_to_file, window_size, stride_size, sample_freq):
     # get data:
-    left_array, right_array = data4_reader.ReadComp4(subject_num, path_to_file)
+    leftX, rightX = data4_reader.ReadComp4(subject_num, path_to_file)
+
+    # create more examples by sliding time window segmentation:
+    #   new size: (num_examples, num_windows, num_channels, window_size)
+    leftX = spectrogram.window_data(leftX, window_size, stride_size)
+    rightX = spectrogram.window_data(rightX, window_size, stride_size)
+
+    # combine first 2 dimensions (num_examples * num_windows):
+    #   new size: (num_examples * num_windows, num_channels, window_size):
+    leftX = np.reshape(leftX, (-1, leftX.shape[2], leftX.shape[3]))
+    rightX = np.reshape(rightX, (-1, rightX.shape[2], rightX.shape[3]))
+
+    # generate class labels:
+    leftY = LEFT_HAND_LABEL * np.ones(leftX.shape[0], dtype=int)
+    rightY = RIGHT_HAND_LABEL * np.ones(rightX.shape[0], dtype=int)
+
+    # concatenate left and right raw data/class labels:
+    X = np.concatenate((leftX, rightX))
+    Y = np.concatenate((leftY, rightY))
