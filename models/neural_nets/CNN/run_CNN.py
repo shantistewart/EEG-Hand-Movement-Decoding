e563cee2722f1b2a97ca228ecd1bee846fb2f717
Shanti Stewart <stewars2@oregonstate.edu>
2020-05-19 00:05:37 -0700

    Renamed file to example_generation.py

--- models/neural_nets/CNN/run_CNN.py
+++ models/neural_nets/CNN/run_CNN.py
@@ -1,1 +1,7 @@
 # This file contains code to run a convolutional neural network.
+
+
+import numpy as np
+# import function modules:
+from models.neural_nets.CNN import example_generation
+from models.feature_calculation import feature_algorithms
