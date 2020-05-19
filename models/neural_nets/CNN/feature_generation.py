1190990b3becb3af33ff393b335a65d67aeca1e2
Shanti Stewart <stewars2@oregonstate.edu>
2020-05-18 23:55:36 -0700

    Updated function comments.

--- models/neural_nets/CNN/feature_generation.py
+++ models/neural_nets/CNN/feature_generation.py
@@ -21,25 +21,25 @@ RIGHT_HAND_LABEL = 1
 #   sample_freq = sampling frequency
 # Outputs:
 #   X = (shuffled) raw data
-#       size: (2 * num_examples * num_windows, num_channels, window_size)
+#       size: (2 * num_trials * num_windows, num_channels, window_size)
 #   Y = (shuffled) class labels
-#       size: (2 * num_examples * num_windows, num_channels, window_size)
+#       size: (2 * num_trials * num_windows, num_channels, window_size)
 def generate_examples(subject_num, path_to_file, window_size, stride_size, sample_freq):
     # convert window and stride sizes from seconds to samples:
     window_size = int(np.floor(sample_freq * window_size))
     stride_size = int(np.floor(sample_freq * stride_size))
 
     # get data:
-    #   size: (num_examples, num_channels, num_samples)
+    #   size: (num_trials, num_channels, num_samples)
     leftX, rightX = data4_reader.ReadComp4(subject_num, path_to_file)
 
     # create more examples by sliding time window segmentation:
-    #   new size: (num_examples, num_windows, num_channels, window_size)
+    #   new size: (num_trials, num_windows, num_channels, window_size)
     leftX = spectrogram.window_data(leftX, window_size, stride_size)
     rightX = spectrogram.window_data(rightX, window_size, stride_size)
 
     # combine first 2 dimensions (num_examples * num_windows):
-    #   new size: (num_examples * num_windows, num_channels, window_size):
+    #   new size: (num_trials * num_windows, num_channels, window_size):
     leftX = np.reshape(leftX, (-1, leftX.shape[2], leftX.shape[3]))
     rightX = np.reshape(rightX, (-1, rightX.shape[2], rightX.shape[3]))
 
