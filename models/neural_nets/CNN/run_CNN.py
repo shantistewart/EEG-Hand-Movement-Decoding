ab11de68c2489320393ca59e3a7b5ec75afa4f3e
Shanti Stewart <stewars2@oregonstate.edu>
2020-05-19 00:22:16 -0700

    Started run_CNN.py

--- models/neural_nets/CNN/run_CNN.py
+++ models/neural_nets/CNN/run_CNN.py
@@ -5,3 +5,29 @@ import numpy as np
 # import function modules:
 from models.neural_nets.CNN import example_generation
 from models.feature_calculation import feature_algorithms
+
+
+# NOT TO BE MODIFIED:
+# path to data files:
+path_to_data_file = "../../../MATLAB/biosig/Data_txt/"
+# sampling frequency:
+sample_freq = 250
+
+
+# subject number:
+subject_num = 1
+
+# HYPERPARAMETERS:
+window_size_example = 2.0
+stride_size_example = 0.1
+
+print("\n")
+
+
+X, Y = example_generation.generate_examples(subject_num, path_to_data_file, window_size_example, stride_size_example,
+                                            sample_freq)
+# display dimensions of raw data:
+print("Size of X: ", end="")
+print(X.shape)
+print("Size of Y: ", end="")
+print(Y.shape)
