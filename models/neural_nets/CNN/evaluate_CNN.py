28e6fd00aa623fee5c26f58d0cdb7d07bed5c157
Shanti Stewart <stewars2@oregonstate.edu>
2020-05-27 22:43:31 -0700

    Created training/validation accuracy dictionaries.

--- models/neural_nets/CNN/evaluate_CNN.py
+++ models/neural_nets/CNN/evaluate_CNN.py
@@ -1,6 +1,7 @@
 # This file contains a function to train/evaluate a convolutional neural network across subjects.
 
 
+import numpy as np
 from models.neural_nets.CNN import conv_neural_net
 
 
@@ -10,5 +11,15 @@ from models.neural_nets.CNN import conv_neural_net
 #       size: (num_subjects, )
 # Outputs:
 def train_eval_CNN(subject_nums):
+    # number of subjects:
+    num_subjects = subject_nums.shape[0]
+    # dictionaries for training and validation accuracies for subjects:
+    train_acc = {}
+    val_acc = {}
+
+    # train a different CNN instance for each subject and record training/validation accuracies:
     for subject in subject_nums:
-        print("Subject {0}\n".format(subject))
+        train_acc[subject] = subject + 0.5
+        val_acc[subject] = subject + 0.1
+
+    return train_acc, val_acc
--- models/neural_nets/CNN/hyperparam_search.py
+++ models/neural_nets/CNN/hyperparam_search.py
@@ -5,7 +5,15 @@ import numpy as np
 from models.neural_nets.CNN import evaluate_CNN
 
 
+print("\n")
+
 # subjects to evaluate:
 subject_nums = np.array([1, 3, 5, 7, 9])
 
-evaluate_CNN.train_eval_CNN(subject_nums)
+# call function:
+train_acc, val_acc = evaluate_CNN.train_eval_CNN(subject_nums)
+print("Training accuracies for subjects:")
+print(train_acc)
+print("")
+print("Validation accuracies for subjects:")
+print(val_acc)
