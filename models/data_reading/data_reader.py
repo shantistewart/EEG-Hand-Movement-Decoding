fadbf7102a65ce521c0d96c593b3bf59076181e4
pmcgrath17 <patarmcgrath17@gmail.com>
2020-07-04 17:28:38 -0700

    Commiting

--- models/data_reading/data_reader.py
+++ models/data_reading/data_reader.py
@@ -1,12 +1,19 @@
+# This file contains a function to read data from data files.
+
+
+import sys
 from os import path
 import numpy as np
-import sys
 
 
-# Keep in mind, the sampling rate is 250Hz.
-# Input Patient number: 1-9
+# Function description: reads EEG data from data files and constructs data arrays.
+# Inputs:
+#   patient_num = number of patient
+#   path_to_file = relative path to data files
 # Outputs:
-#   left_array, right_array
+#   left_array = 3D array of left-hand data
+#       size: (num_trials, num_channels, num_samples)
+#   right_array = 3D array of right-hand data
 #       size: (num_trials, num_channels, num_samples)
 def ReadComp4(patient_num, path_to_file):
     if patient_num < 0 or patient_num > 9:
@@ -14,39 +21,27 @@ def ReadComp4(patient_num, path_to_file):
 
     left_file_form = path_to_file + "Data_Left_" + str(patient_num) + "_"
     right_file_form = path_to_file + "Data_Right_" + str(patient_num) + "_"
-    unlabeled_file_form = path_to_file + "Data_Unlabeled_" + str(patient_num) + "_"
 
     left_array = []
     right_array = []
-    unlabeled_array = []
     for i in range(1, 6):
         for j in range(1, 81):
             left_file = left_file_form + str(i) + "_" + str(j) + ".txt"
             right_file = right_file_form + str(i) + "_" + str(j) + ".txt"
             if path.exists(left_file):
                 left_array += [np.loadtxt(left_file)]
-            #else:
-            #    print("Left file is not available %d %d %d" %(patient_num, i, j))
             if path.exists(right_file):
                 right_array += [np.loadtxt(right_file)]
-            #else:
-            #    print("Right file is not available %d %d %d" %(patient_num, i, j))
-        for l in range(1, 161):
-            unlabeled_file = unlabeled_file_form + str(i) + "_" + str(l) + ".txt"
-            if path.exists(unlabeled_file):
-                unlabeled_array += [np.loadtxt(unlabeled_file)]
     left_array = np.array(left_array)
     right_array = np.array(right_array)
-    unlabeled_array = np.array(unlabeled_array)
     left_array = np.transpose(left_array, (0, 2, 1))
     right_array = np.transpose(right_array, (0, 2, 1))
-    unlabeled_array = np.transpose(unlabeled_array, (0, 2, 1))
 
-    return left_array, right_array, unlabeled_array
+    return left_array, right_array
 
-# Stride and window are in seconds! Note that
-# stride is the start time for a particular window.
-# The window_len is the length of the window
+
+# Stride and window are in seconds! Note that stride is the start time for a particular window.
+# The window_len is the length of the window.
 # Output:
 #   A 3D array of dimension: (strides, channels, window_len)
 def stride_window(eeg_trial, stride, window_len, frequency):
@@ -56,7 +51,7 @@ def stride_window(eeg_trial, stride, window_len, frequency):
     stride_examples = int(stride * frequency)
     window_examples = int(window_len * frequency)
     num_strides = eeg_trial.shape[1] // stride_examples
-    # If window size is too long, need to cut back on number of stride
+    # if window size is too long, need to cut back on number of stride"
     while (num_strides * stride_examples) + window_examples > eeg_trial.shape[1]:
         num_strides = num_strides - 1
 
